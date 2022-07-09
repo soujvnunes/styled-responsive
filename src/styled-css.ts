cf85ec5bcb5f9b49168ceb3ad98aed32d6724f19
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-07-09 18:29:26 -0300

    Eval when loops are necessary

--- src/styled-css.ts
+++ src/styled-css.ts
@@ -31,17 +31,20 @@ export type StyledCssProps<
   }
 }
 
+const refIniVal = {
+  hasRaw: false,
+  rawCounter: 0,
+  rawTemplate: {},
+  template: {}
+}
+
 function cssObj<K extends string, V>(key: K, value?: V) {
   return {
     [key]: value
   }
 }
 function styledCss(args: StyledCssProps) {
-  const ref = {
-    hasRaw: false,
-    raw: {},
-    template: {}
-  }
+  const ref = refIniVal
 
   for (const keyUntyped in args.css) {
     if (Object.prototype.hasOwnProperty.call(args.css, keyUntyped)) {
@@ -49,6 +52,8 @@ function styledCss(args: StyledCssProps) {
       const value = args.css[key]
 
       if (typeof value === 'string') {
+        ref.hasRaw = refIniVal.hasRaw
+        ref.rawCounter = refIniVal.rawCounter
         ref.template = {
           ...ref.template,
           ...cssObj(key, value)
@@ -64,10 +69,15 @@ function styledCss(args: StyledCssProps) {
                 ...cssObj(key, value[mediaKey])
               }
             } else {
-              ref.raw = {
-                ...ref.raw,
+              if (!ref.rawCounter) {
+                ref.hasRaw = true
+                ref.rawCounter += 1
+              }
+
+              ref.rawTemplate = {
+                ...ref.rawTemplate,
                 [mediaKey]: {
-                  ...(ref.raw as ResponsiveProp<object>)[mediaKey],
+                  ...(ref.rawTemplate as ResponsiveProp<object>)[mediaKey],
                   [key]: value[mediaKey]
                 }
               }
@@ -78,16 +88,18 @@ function styledCss(args: StyledCssProps) {
     }
   }
 
-  ref.template = Object.keys(ref.raw).reduce((prev, _curr) => {
-    const key = _curr as MediaThemeKs
+  if (ref.hasRaw) {
+    ref.template = Object.keys(ref.rawTemplate).reduce((prev, _curr) => {
+      const key = _curr as MediaThemeKs
 
-    return css`
-      ${prev};
-      ${(props: ThemeProps<DefaultTheme>) => props.theme.media[key]} {
-        ${(ref.raw as ResponsiveProp)[key]};
-      }
-    `
-  }, ref.template)
+      return css`
+        ${prev};
+        ${(props: ThemeProps<DefaultTheme>) => props.theme.media[key]} {
+          ${(ref.rawTemplate as ResponsiveProp)[key]};
+        }
+      `
+    }, ref.template)
+  }
 
   return css`
     ${ref.template}
